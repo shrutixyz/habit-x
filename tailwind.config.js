e0ea6a47fd5bb63b62a7d11e202e3e7c8ec2a217
Vamiq <vamiq.valji@gmail.com>
2021-05-22 01:58:17 -0400

    added colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,22 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        accent : '#FFE2E9',
-        midaccent : '#F77CA8',
-        darkaccent : '#FCE8D4',
-        lightaccent : "rgba(198, 27, 74, 0.5)",
+        accent: "#FFE2E9",
+        midaccent: "#F77CA8",
+        darkaccent: "#FCE8D4",
+        lightaccent: "rgba(198, 27, 74, 0.5)",
         lightgray: "#F3F3FB",
-        midgray: '#FDFBFD'
-        
+        midgray: "#FDFBFD",
+        rc: "#FF0A54",
+        fr: "#FF477E",
+        bp: "#FF5C8A",
+        fp: "#FF7096",
+        ps: "#FF85A1",
+        sp: "#FF99AC",
+        lp: "#FBB1BD",
+        p: "#F9BEC7",
+        bp: "#F7CAD0",
+        pp: "#FAE0E4",
       },
     },
   },
@@ -18,4 +27,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};
