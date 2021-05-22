7a2718738db1db8a6156b7809924134812cc5707
shruti <shrutigupta5555@gmail.com>
2021-05-22 10:45:42 +0530

    ADD: Color config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        accent : '#FFE2E9',
+        midaccent : '#F77CA8',
+        darkaccent : '#FCE8D4',
+        lightaccent : "rgba(198, 27, 74, 0.5)",
+        lightgray: "#F3F3FB",
+        midgray: '#FDFBFD'
+        
+      },
+    },
   },
   variants: {
     extend: {},
