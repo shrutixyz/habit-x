fa76b9a3b175adfa84744fa655a0dabe4f723fc0
Vamiq <vamiq.valji@gmail.com>
2021-05-22 17:29:21 -0400

    added more styling to login page

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from "react";
 import { auth } from "../../utils/firebase";
 import { useHistory } from "react-router-dom";
 import "./Login.scss";
+import { ReactComponent as OtherGirl } from "../../svg/OtherGirl.svg";
+import { ReactComponent as Angela } from "../../svg/Angela.svg";
+import { ReactComponent as CreativeBlock } from "../../svg/Creative-Block.svg";
 
 function Register() {
   const [email, setemail] = useState("");
@@ -23,49 +26,63 @@ function Register() {
       });
   };
   return (
-    <div className="flex">
-      <form className=" mt-10 md:w-2/3 shadow-md px-5 py-3 max-w-lg ">
-        <div class="w-full px-3 py-3">
-          <label
-            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            for="grid-last-name"
-          >
-            Email
-          </label>
-          <input
-            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-            id="grid-last-name"
-            type="email"
-            placeholder="Enter Email"
-            value={email}
-            onChange={(e) => setemail(e.target.value)}
-          />
+    <div className="leftRightContainer">
+      <div className="left">
+        <div className="leftInnerContainer">
+        <div className="titleContainer">
+            <h1 clasName="header">LOGIN</h1>
         </div>
+          <form className="formContainer mt-10 md:w-2/3 shadow-md px-5 py-3 max-w-lg ">
+            <div class="w-full px-3 py-3">
+              <label
+                class="eblock uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                for="grid-last-name"
+              >
+                Email
+              </label>
+              <input
+                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                id="grid-last-name"
+                type="email"
+                placeholder="Enter Email"
+                value={email}
+                onChange={(e) => setemail(e.target.value)}
+              />
+            </div>
 
-        <div class="w-full px-3 py-3">
-          <label
-            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            for="grid-last-name"
-          >
-            Password
-          </label>
-          <input
-            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-            id="grid-last-name"
-            type="text"
-            placeholder="Enter Password"
-            value={pass}
-            onChange={(e) => setpass(e.target.value)}
-          />
+            <div class="w-full px-3 py-3">
+              <label
+                class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                for="grid-last-name"
+              >
+                Password
+              </label>
+              <input
+                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                id="grid-last-name"
+                type="text"
+                placeholder="Enter Password"
+                value={pass}
+                onChange={(e) => setpass(e.target.value)}
+              />
+            </div>
+
+            <div className="buttonContainer">
+                <button
+                className="bg-fr m-auto mx-4 text-white px-5 py-2"
+                onClick={login}
+                >
+                Log In
+                </button>
+            </div>
+          </form>
         </div>
-
-        <button
-          className="bg-fr m-auto mx-4 text-white px-5 py-2"
-          onClick={login}
-        >
-          Log In
-        </button>
-      </form>
+      </div>
+      <div className="right">
+        <OtherGirl className="otherGirl" />
+        <Angela className="angela" />
+        <CreativeBlock className="" />
+      </div>
     </div>
   );
 }
--- src/pages/Onboarding/Onboarding.js
+++ src/pages/Onboarding/Onboarding.js
@@ -1,21 +1,20 @@
 import "./Onboarding.scss";
-import {ReactComponent as OtherGirl} from './svg/OtherGirl.svg';
-import {ReactComponent as Angela} from './svg/Angela.svg';
-import {ReactComponent as CreativeBlock} from './svg/Creative-Block.svg';
-import {Link} from "react-router-dom";
-
-
+import { ReactComponent as OtherGirl } from "../../svg/OtherGirl.svg";
+import { ReactComponent as Angela } from "../../svg/Angela.svg";
+import { ReactComponent as CreativeBlock } from "../../svg/Creative-Block.svg";
+import { Link } from "react-router-dom";
 
 const Onboarding = () => {
   return (
     <>
-  
       <div className="leftRightContainer">
         <div className="left">
           <div className="leftInnerContainer">
             <h1 className="">Habit X</h1>
             <p className="">Helping You And Your Friends Start New Habits.</p>
-            <Link to="/login"><button>Get Started</button></Link>
+            <Link to="/login">
+              <button>Get Started</button>
+            </Link>
           </div>
         </div>
         <div className="right">
