0468d7a0ae14f717babc9e603b816816b8f77e95
Vamiq <vamiq.valji@gmail.com>
2021-05-22 02:28:15 -0400

    added react router

--- src/App.js
+++ src/App.js
@@ -1,46 +1,23 @@
 import "./App.scss";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Onboarding from "./pages/Onboarding";
+import Nav from "./components/Nav";
 
 function App() {
   return (
-    <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#" id="titlename">
-            HABIT X
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0"></ul>
-            <form className="d-flex">
-              <button
-                className="btn btn-outline-success"
-                href="#"
-                id="loginbtn"
-              >
-                LOGIN
-              </button>
-              <button
-                className="btn btn-outline-success"
-                href="#"
-                id="signupbtn"
-              >
-                SIGNUP
-              </button>
-            </form>
-          </div>
-        </div>
-      </nav>
-    </>
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          {/* <Route path="/about">
+            <About />
+          </Route> */}
+          <Route exact path="/">
+            <Onboarding />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
