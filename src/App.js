556a6c65376d7cd0743715d219a9a6812aae7366
aakzsh <69726390+aakzsh@users.noreply.github.com>
2021-05-22 10:12:47 +0530

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 
-import './index.css';
+import './App.css';
 
 function App() {
   return (
-<div className="App">
-<header className="App-header">
-{/* <img src = {logo} className="App-logo" alt="logo"/> */}
-<p>Edit <code>src/App.js</code> and save to reload heheheh</p>
-<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-  learn React
-</a>
-</header>
-</div>
+    <>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#" id="titlename">HABIT X</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        
+       
+          
+         
+       
+         
+      </ul>
+      <form className="d-flex">
+      <button className="btn btn-outline-success" href="#" id="loginbtn" >LOGIN</button>
+    
+            <button className="btn btn-outline-success" href = "#" id="signupbtn">SIGNUP</button>
+      </form>
+    </div>
+  </div>
+</nav>
+  </>
   );
 }
 
