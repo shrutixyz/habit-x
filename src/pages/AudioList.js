476152920ce8cfa66880ab43347d465edacdcf68
Vamiq <vamiq.valji@gmail.com>
2021-05-23 02:50:41 -0400

    merge

--- src/pages/AudioList.js
+++ src/pages/AudioList.js
@@ -1,50 +1,56 @@
-import React, {useState, useEffect} from 'react';
-import AudioPodcast from '../components/AudioPodcast'
-import {useHistory} from 'react-router-dom';
-import SearchIcon from '@material-ui/icons/Search';
-import Audio from './Audio'
+import React, { useState, useEffect } from "react";
+import AudioPodcast from "../components/AudioPodcast";
+import { useHistory } from "react-router-dom";
+import SearchIcon from "@material-ui/icons/Search";
+import Audio from "./Audio";
 
 function AudioList() {
-    const list = [
-        {
-            id : "1",
-            mp3Url: "../assets/harry-styles.mp3",
-            imgUrl: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP._yAzxPxW9ZXdr2Bp7Ujh8wHaHQ%26pid%3DApi&f=1",
-            artist: "Harry styles",
-            title: "Dream With Me"
-        }
-    ]
+  const list = [
+    {
+      id: "1",
+      mp3Url: "../assets/harry-styles.mp3",
+      imgUrl:
+        "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP._yAzxPxW9ZXdr2Bp7Ujh8wHaHQ%26pid%3DApi&f=1",
+      artist: "Harry styles",
+      title: "Dream With Me",
+    },
+  ];
 
-    const history = useHistory();
-    const [url, seturl] = useState("")
+  const history = useHistory();
+  const [url, seturl] = useState("");
 
-    const navigate = (_id, _mp3Url) => {
-        history.push(`/audio-podcast/${_id}`)
-    }
+  const navigate = (_id, _mp3Url) => {
+    history.push(`/audio-podcast/${_id}`);
+  };
 
-    const playonyt = (url) => {
-        
-        const vid = url.split("=")[1]
-        history.push(`/audio/${vid}`)
-    }
+  const playonyt = (url) => {
+    const vid = url.split("=")[1];
+    history.push(`/audio/${vid}`);
+  };
 
-    return (
-        <div>
-            <div className="justify-between px-10 md:px-40 lg:px-60 py-24">
-                <input type="text" className="bg-purple-white w-80 shadow rounded border-0 p-3" value={url} placeholder="Enter youtube url..." onChange={(e) => {seturl(e.target.value)}}/>
-                <div className="bg-midaccent sm:p-3 sm:ml-4 p-2 ml-1 inline-block">
-                    <SearchIcon className="text-xl" onClick={() => playonyt(url)}/>
-                </div>
-            </div>
-            {/* <div className="">
+  return (
+    <div>
+      <div className="justify-between px-10 md:px-40 lg:px-60 py-24">
+        <input
+          type="text"
+          className="bg-purple-white w-80 shadow rounded border-0 p-3"
+          value={url}
+          placeholder="Enter youtube url..."
+          onChange={(e) => {
+            seturl(e.target.value);
+          }}
+        />
+        <div className="bg-midaccent sm:p-3 sm:ml-4 p-2 ml-1 inline-block">
+          <SearchIcon className="text-xl" onClick={() => playonyt(url)} />
+        </div>
+      </div>
+      {/* <div className="">
                 {list.map(item => {
                     return <AudioPodcast key={item.id} navigate={navigate} {...item}/>
                 })}
             </div> */}
-           
-            
-        </div>
-    )
+    </div>
+  );
 }
 
-export default AudioList
+export default AudioList;
--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,87 +1,102 @@
 import "./Dashboard.scss";
-import {ReactComponent as OtherGirl} from './svg/OtherGirl.svg';
-import {ReactComponent as Angela} from './svg/Angela.svg';
-import {ReactComponent as CreativeBlock} from './svg/Creative-Block.svg';
-import {Link} from "react-router-dom";
+import { ReactComponent as OtherGirl } from "./svg/OtherGirl.svg";
+import { ReactComponent as Angela } from "./svg/Angela.svg";
+import { ReactComponent as CreativeBlock } from "./svg/Creative-Block.svg";
+import { Link } from "react-router-dom";
 // import Calender as Calender;
 import NewHabit from "../NewHabit/NewHabit";
 import Calendar from "react-calendar";
 
-
 const dashboard = () => {
-  var name = "Christy Grant"
-    return (
-      <>
-      <div className="fullscreen">
-        <div className= "col1">
-
-<center><Angela className="Angela" width="60%"/></center>
-<center><div className="list">
-  <p>{name}</p><br/><br/>
-  <button className="btn">HOME</button><br/>
-  <button className="btn">MY PROFILE</button><br/>
-  <button className="btn">NOTIFICATIONS</button><br/>
-  <button className="btn">CALENDAR</button><br/>
-  <button className="btn">SETTINGS</button><br/>
-  <button className="btn">LOG OUT</button><br/>
-  
-  </div></center>
+  var name = "Christy Grant";
+  return (
+    <>
+      <div className="fullscreen dashboardContainer">
+        <div className="col1">
+          <center>
+            <Angela className="Angela" width="60%" />
+          </center>
+          <center>
+            <div className="list">
+              <p>{name}</p>
+              <br />
+              <br />
+              <button className="btn">HOME</button>
+              <br />
+              <button className="btn">MY PROFILE</button>
+              <br />
+              <button className="btn">NOTIFICATIONS</button>
+              <br />
+              <button className="btn">CALENDAR</button>
+              <br />
+              <button className="btn">SETTINGS</button>
+              <br />
+              <button className="btn">LOG OUT</button>
+              <br />
+            </div>
+          </center>
         </div>
-
       </div>
-      
-<div className="col2">
-<p className="welcome">Welcome back!</p>
-<p className="subtitle">Recent Notifications</p>
-<Link to="/newhabit"><button className="btn3">Start New Habit</button></Link>
 
+      <div className="col2">
+        <p className="welcome">Welcome back!</p>
+        <p className="subtitle">Recent Notifications</p>
+        <Link to="/newhabit">
+          <button className="btn3">Start New Habit</button>
+        </Link>
 
-<div className="todolist">
-  <p>📝 Your Daily Habits</p><br/>
-  <input type="checkbox" id="checkbox" name="task1" value="task1"/>
-  <label for="task1">Take run outside for 30 minutes</label><br/><br/>
-  <input type="checkbox" id="checkbox" name="task2" value="task2"/>
-  <label for="task2">Cook a home cook meal</label><br/><br/>
-  <input type="checkbox" id="checkbox" name="task3" value="task3"/>
-  <label for="task3">Practice violin for 60-80 minutes</label><br/><br/>
-
-</div>
-
-
-
-<div className="messages">
-
-  <table>
-  <td><img src="https://i.ibb.co/gTPj2s4/29-17.png"></img></td>
-    <th><p>Sarah</p> 
-  <p>finished Cook a home cook meal</p>
-  <p>(2 mins ago)</p></th>
-  
-  </table>
-
-</div>
-</div>
-<div className= "col3">
-<div className="tracker">
-  <p>💪 Progress Tracker</p>
-  <center><img src="https://i.ibb.co/2tCkcnL/Timer.png"></img></center>
- <center> <p>Take run outside for 30 minutes</p>
-  <p>keep up the good work!</p></center>
-</div>
-<div className="awards">
- <center> <p>Your Awards</p></center>
-  <div className="awards-graph">
-  </div>
-
-</div>
+        <div className="todolist">
+          <p>📝 Your Daily Habits</p>
+          <br />
+          <input type="checkbox" id="checkbox" name="task1" value="task1" />
+          <label for="task1">Take run outside for 30 minutes</label>
+          <br />
+          <br />
+          <input type="checkbox" id="checkbox" name="task2" value="task2" />
+          <label for="task2">Cook a home cook meal</label>
+          <br />
+          <br />
+          <input type="checkbox" id="checkbox" name="task3" value="task3" />
+          <label for="task3">Practice violin for 60-80 minutes</label>
+          <br />
+          <br />
         </div>
-      </>
-    );
-}
+
+        <div className="messages">
+          <table>
+            <td>
+              <img src="https://i.ibb.co/gTPj2s4/29-17.png"></img>
+            </td>
+            <th>
+              <p>Sarah</p>
+              <p>finished Cook a home cook meal</p>
+              <p>(2 mins ago)</p>
+            </th>
+          </table>
+        </div>
+      </div>
+      <div className="col3">
+        <div className="tracker">
+          <p>💪 Progress Tracker</p>
+          <center>
+            <img src="https://i.ibb.co/2tCkcnL/Timer.png"></img>
+          </center>
+          <center>
+            {" "}
+            <p>Take run outside for 30 minutes</p>
+            <p>keep up the good work!</p>
+          </center>
+        </div>
+        <div className="awards">
+          <center>
+            {" "}
+            <p>Your Awards</p>
+          </center>
+          <div className="awards-graph"></div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default dashboard;
-
-
-
-
-
