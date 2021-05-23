878c0c9b1cd5a318fd3e9ecce0577f51856c326f
Vamiq <vamiq.valji@gmail.com>
2021-05-22 21:53:39 -0400

    added conditional tab rendering on tab click

--- src/pages/HabitSocial/HabitSocial.js
+++ src/pages/HabitSocial/HabitSocial.js
@@ -4,8 +4,31 @@ import {ReactComponent as FriendOne} from './svg/2917.svg';
 import {ReactComponent as FriendTwo} from './svg/2929.svg';
 import {ReactComponent as FriendThree} from './svg/2923.svg';
 import "./HabitSocial.scss";
+import React, { useState } from "react";
 
 const HabitSocial = () => {
+
+    const [currentTab, setCurrentTab] = useState("notes"); // 'notes' or 'progress'
+
+    const renderTab = () => {
+        if (currentTab === "notes") {
+            return (
+                <>
+                    <div className="notesContainer">
+                        notes
+                    </div>
+                </>
+            );
+        } else if (currentTab === "progress")
+        return (
+            <>
+                <div className="progressContainer">
+                    progress
+                </div>
+            </>
+        );
+    };
+
     return (
         <>
             <div className="container">
@@ -20,7 +43,7 @@ const HabitSocial = () => {
                         <div className="textContent">
                             <h3>HABIT</h3>
                             <h2>Read 1-2 Chapters of a Book 📖</h2>
-                            <p>Let’s get in the habit of practicing Leetcode to prep for Lets expand our knowledge by reading some books!!</p>
+                            <p>Lets expand our knowledge by reading some books!!</p>
                             <div className="friendsContainer">
                                 <div className="friends">
                                     <FriendOne />
@@ -30,13 +53,17 @@ const HabitSocial = () => {
                                 <div className="paraContainer">
                                     <p>3 friends are already done today.</p>
                                 </div>
-                                
                             </div>
                         </div>
                     </div>
-                    <div className="tabs">
-                        <span>Notes</span>
-                        <span>Progress</span>
+                    <div className="tabsContainer">
+                        <div className="tabs">
+                            <span onClick={(() => setCurrentTab("notes"))} className={currentTab === "notes" ? "selectedTab" : "notSelectedTab"}>NOTES</span>
+                            <span onClick={(() => setCurrentTab("progress"))} className={currentTab === "progress" ? "selectedTab" : "notSelectedTab"}>PROGRESS</span>
+                        </div>
+                        <div className="tab">
+                            {renderTab()}
+                        </div>
                     </div>
                 </div>
 
